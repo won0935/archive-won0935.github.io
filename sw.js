72296e2630bd28a43668a678c52befb38031a606
won0935 <won0935@gmail.com>
2022-01-08 12:31:53 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-779cc88c5dc63083efb9.js"
+    "url": "webpack-runtime-4646e9be2743e38d3e9e.js"
   },
   {
     "url": "framework-999a01e9e4c894cffffe.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "efc9f540d8fa0ad864f3776ccb266206"
+    "revision": "290ab1ecd78270d3a9b5ebaab5307e18"
   },
   {
     "url": "polyfill-6ed0469530c21b23fd71.js"
